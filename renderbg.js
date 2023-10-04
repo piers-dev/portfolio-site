26a3a2857347e0f15a73876cbdda172dc8d2c6ee
Pixstatic <55572014+Pixstatic@users.noreply.github.com>
2023-10-05 08:42:30 +1000

    compensation

--- renderbg.js
+++ renderbg.js
@@ -40,8 +40,8 @@ function draw() {
     // shader() sets the active shader, which will be applied to what is drawn next
     shader(myShader);
 
-    myShader.setUniform('screenSize', [1,height/width]);
-    myShader.setUniform('mousePos', [1.-mouseX/width,(mouseY/height)*height/width]);
+    myShader.setUniform('screenSize', (width>height) ? [1,height/width] : [width/height,1]);
+    //myShader.setUniform('mousePos', [1.-mouseX/width,(mouseY/height)*height/width]);
     myShader.setUniform('angle',[-0.3]);
 
 
